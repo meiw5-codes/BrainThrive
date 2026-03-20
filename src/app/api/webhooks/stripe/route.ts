a62d60a61b64f12f6d15a3291b9bc486fd0987e9
Mei Wu <meiwu@Meis-MacBook-Pro.local>
2026-03-19 23:11:53 -0700

    Fix Stripe API version

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -12,7 +12,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-02-25.clover",
 });
 
 export async function POST(req: NextRequest) {
